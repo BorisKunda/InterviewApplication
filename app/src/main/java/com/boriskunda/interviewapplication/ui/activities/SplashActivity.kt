cc27ca37e02d42b3dc804bd19007511881140807
BorisKunda <bskunda1992@gmail.com>
2020-06-22 14:11:02 +0300

    Added count down logic to splash activity

--- app/src/main/java/com/boriskunda/interviewapplication/ui/activities/SplashActivity.kt
+++ app/src/main/java/com/boriskunda/interviewapplication/ui/activities/SplashActivity.kt
@@ -2,31 +2,96 @@ package com.boriskunda.interviewapplication.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
+import android.os.CountDownTimer
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.boriskunda.interviewapplication.R
+import com.boriskunda.interviewapplication.utilities.SPLASH_COUNT_INTERVAL
 import com.boriskunda.interviewapplication.utilities.SPLASH_DURATION
 import com.boriskunda.interviewapplication.viewmodel.SharedViewModel
 
 class SplashActivity : AppCompatActivity() {
 
     private val sharedViewModel: SharedViewModel by viewModels()
+    private lateinit var countDownTimer: CountDownTimer
+    private lateinit var alertDialog: AlertDialog
+    private var areMoviesStoredInDb: Boolean = false
+    private var temp: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-//        Handler().postDelayed({
-//
-//            Intent(this@SplashActivity, MainActivity::class.java).let {
-//                startActivity(it)
-//                finish()
-//            }
-//
-//        }, SPLASH_DURATION)
-
-        sharedViewModel.loadMovies()
+        setErrorDialog()
+        initCountDown()
+        observeViewModel(sharedViewModel)
 
     }
+
+    private fun observeViewModel(vm: SharedViewModel) {
+        vm.moviesListLD.observe(this, Observer {
+
+            if (it.isEmpty()) {
+                vm.loadMovies()
+            } else {
+                areMoviesStoredInDb = true
+            }
+
+        })
+    }
+
+    private fun initCountDown() {
+
+        countDownTimer = object : CountDownTimer(SPLASH_DURATION, SPLASH_COUNT_INTERVAL) {
+            override fun onFinish() {
+
+                runOnUiThread {
+
+                    if (areMoviesStoredInDb) {
+                        val intent = Intent(
+                            this@SplashActivity,
+                            MainActivity::class.java
+                        )
+                        startActivity(intent)
+                        cancel()
+                        finish()
+                    } else {
+                        alertDialog.show()
+                        cancel()
+                    }
+
+                }
+
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                //do nothing
+            }
+
+        }.start()
+
+    }
+
+    private fun setErrorDialog() {
+        val builder = AlertDialog.Builder(this@SplashActivity)
+        //Retry
+        builder.setPositiveButton(getString(R.string.Dialog_Positive_Button_Text)) { _, _ ->
+            sharedViewModel.loadMovies()
+            countDownTimer.start()
+        }
+        //Close
+        builder.setNegativeButton(getString(R.string.Dialog_Negative_Button_Text)) { _, _ ->
+            finish()
+        }
+
+        alertDialog = builder.create().apply {
+            setIcon(R.drawable.ic__error)
+            setTitle(getString(R.string.Dialog_Title_Error))
+            setMessage(getString(R.string.Dialog_Message_Error))
+        }
+
+    }
+
 }
